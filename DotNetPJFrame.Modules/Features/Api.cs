d6979a3df9c7ccc2281e08e19d44773bd60d1554
minthukha <minthukha1239788@gmail.com>
2024-07-15 00:35:54 +0630

    [ Added ] - Modules OF Api Service

---
+++ DotNetPJFrame.Modules/Features/Api.cs
@@ -0,0 +1,23 @@
+﻿using DotNetPJFrame.Modules.Features.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetPJFrame.Modules.Features;
+
+public class Api
+{
+    private readonly BL_Test _blTest;
+
+    public Api(BL_Test blTest)
+    {
+        _blTest = blTest;
+    }
+
+    public async Task<bool> Test()
+    {
+        return await _blTest.Test();
+    }
+}
---
+++ DotNetPJFrame.Modules/Features/ApiGate.cs
@@ -0,0 +1,30 @@
+﻿using DotNetPJFrame.Models.Enum;
+using DotNetPJFrame.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetPJFrame.Modules.Features;
+
+public class ApiGate
+{
+    private readonly Api _api;
+
+    public ApiGate(Api api)
+    {
+        _api = api;
+    }
+
+    public async Task<object>Service(string serviceName,string raw)
+    {
+        object result = null;
+        var name = serviceName.ToEnum<EnumApi>();
+        result = name switch
+        {
+            EnumApi.TEST => _api.Test()
+        };
+        return result;
+    }
+}
