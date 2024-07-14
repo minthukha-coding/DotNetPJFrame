0f22440ad5611e29a7ef32fccef8f84896c0471b
minthukha <minthukha1239788@gmail.com>
2024-07-15 00:33:28 +0630

    [ InProgress ]  - ApiController

---
+++ DotNetPJFrame.Api/Features/Api/ApiController.cs
@@ -0,0 +1,30 @@
+﻿using DotNetPJFrame.Api.Features.Gate;
+using DotNetPJFrame.Models.Enum.Common;
+using DotNetPJFrame.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetPJFrame.Api.Features.Api;
+
+[Route("api")]
+[ApiController]
+public class ApiController : ControllerBase
+{
+    private readonly ILogger<ApiController> logger;
+    private readonly MainApiGate _mainApiGate;
+
+    public ApiController(ILogger<ApiController> logger,
+        MainApiGate mainApiGate)
+    {
+        this.logger = logger;
+        _mainApiGate = mainApiGate;
+    }
+
+    [HttpPost]
+    [Route("Service")]
+    public async Task<IActionResult> Service([FromBody] ApiRequestModel reqModel)
+    {
+        var model = await _mainApiGate.ExecuteAsync(reqModel);
+        return Content(model.ToJson() ?? throw new InvalidOperationException(), "application/json");
+    }
+}
