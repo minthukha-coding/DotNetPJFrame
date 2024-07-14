9fa25e3f0402e01122ce760164e803a1df72bddc
minthukha <minthukha1239788@gmail.com>
2024-07-15 00:35:11 +0630

    [ testing ] - api of feature

---
+++ DotNetPJFrame.Modules/Features/Test/BL_Test.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetPJFrame.Modules.Features.Test;
+
+public class BL_Test
+{
+    private readonly ILogger<BL_Test> logger;
+    private readonly DA_Test _daTest;
+
+    public BL_Test(ILogger<BL_Test> logger, 
+        DA_Test daTest)
+    {
+        this.logger = logger;
+        _daTest = daTest;
+    }
+
+    public async Task<bool> Test()
+    {
+        return false;
+    }
+}
---
+++ DotNetPJFrame.Modules/Features/Test/DA_Test.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetPJFrame.Modules.Features.Test;
+
+public class DA_Test
+{
+    private readonly ILogger<DA_Test> logger;
+
+    public DA_Test(ILogger<DA_Test> logger)
+    {
+        this.logger = logger;
+    }
+
+    public async Task<bool> Test()
+    {
+        return false;
+    }
+}
