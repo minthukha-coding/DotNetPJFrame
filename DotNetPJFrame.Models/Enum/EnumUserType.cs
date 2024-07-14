e674c7a1e11987b95e23293a8e6e2b4324ff3ea1
minthukha <minthukha1239788@gmail.com>
2024-07-14 15:49:10 +0630

    [ Added ] - Enum

--- DotNetPJFrame.Models/Enum/EnumUserType.cs
+++ DotNetPJFrame.Models/Enum/EnumUserType.cs
@@ -5,24 +5,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DotNetPJFrame.Models.Enum
+namespace DotNetPJFrame.Models.Enum;
+
+public enum EnumUserType
 {
-    public enum EnumUserType
-    {
-        Default,
-        [Description("Wallet")]
-        Wallet,
-        [Description("Retail")]
-        Retail,
-        [Description("Corporate")]
-        Corporate,
-        [Description("Agent")]
-        Agent,
-        [Description("Teller")]
-        Teller,
-        Admin,
-        System,
-        Mobile,
-        Merchant
-    }
+    Default,
+    [Description("Wallet")]
+    Wallet,
+    [Description("Retail")]
+    Retail,
+    [Description("Corporate")]
+    Corporate,
+    [Description("Agent")]
+    Agent,
+    [Description("Teller")]
+    Teller,
+    Admin,
+    System,
+    Mobile,
+    Merchant
 }
