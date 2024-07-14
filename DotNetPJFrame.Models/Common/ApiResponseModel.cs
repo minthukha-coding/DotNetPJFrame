94981b29c338957eaba1c54cf8634c39a1863578
minthukha <minthukha1239788@gmail.com>
2024-07-14 15:54:59 +0630

    -

---
+++ DotNetPJFrame.Models/Common/ApiResponseModel.cs
@@ -0,0 +1,21 @@
+﻿using DotNetPJFrame.Models.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetPJFrame.Models.Common;
+
+public class ApiResponseModel
+{
+    public ResponseModel? Response { get; set; }
+    public string ReqService { get; set; }
+    public DateTime ReqDate { get; set; }
+    public DateTime RespDate { get; set; }
+    public EnumUserType UserType { get; set; }
+    public string ProcessTime { get; set; }
+    public string? IV { get; set; }
+    public object RespData { get; set; }
+    public string Token { get; set; }
+}
---
+++ DotNetPJFrame.Models/Common/BaseSubResponseModel.cs
@@ -0,0 +1,28 @@
+﻿using DotNetPJFrame.Models.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetPJFrame.Models.Common;
+
+public class BaseSubResponseModel
+{
+    public ResponseModel Response { get; set; } = new ResponseModel();
+    public string RefID { get; set; }
+    public int TotalRowCount { get; set; }
+    //public EnumUserType UserType { get; set; }
+}
+
+public class ResponseModel
+{
+    public string RespCaseId { get; set; }
+    public string RespCode { get; set; }
+    public string RespDesp { get; set; }
+    public int? UpdateCount { get; set; }
+    public EnumRespType RespType { get; set; }
+    public bool IsSuccess => RespType == EnumRespType.MS;
+    public bool IsError => !IsSuccess;
+    public object[]? RespDespParameter { get; set; } = new string[] { };
+}
