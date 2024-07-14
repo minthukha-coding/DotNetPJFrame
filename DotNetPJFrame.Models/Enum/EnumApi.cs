b3f427f2d280c12cfb29d97084e2c077e389a475
minthukha <minthukha1239788@gmail.com>
2024-07-15 00:34:32 +0630

    [ Added ] - Enum

---
+++ DotNetPJFrame.Models/Enum/EnumApi.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetPJFrame.Models.Enum;
+
+public enum EnumApi
+{
+    TEST
+}
