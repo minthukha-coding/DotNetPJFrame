9b60556be62044caf7fdecb8372e09afc0b87f68
minthukha <minthukha1239788@gmail.com>
2024-07-15 00:34:09 +0630

    [ Add ] - Program.cs in service

--- DotNetPJFrame.Api/Program.cs
+++ DotNetPJFrame.Api/Program.cs
@@ -1,3 +1,8 @@
+using DotNetPJFrame.Api.Features.Gate;
+using DotNetPJFrame.Modules.Features;
+using DotNetPJFrame.Modules.Features.Test;
+using Microsoft.AspNetCore.Authorization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,26 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<MainApiGate>();
+
+#region MerchantApi,Gate
+
+builder.Services.AddScoped<ApiGate>();
+builder.Services.AddScoped<Api>();
+#endregion
+
+#region BL
+
+builder.Services.AddScoped<BL_Test>();
+
+#endregion
+
+#region DA
+
+builder.Services.AddScoped<DA_Test>();
+
+#endregion
 
 var app = builder.Build();
 
