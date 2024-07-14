0375dfb4e37ed99268b30105d5a0be3b4201b4f3
minthukha <minthukha1239788@gmail.com>
2024-07-14 15:55:03 +0630

    -

---
+++ DotNetPJFrame.Models/Enum/EnumRespType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetPJFrame.Models.Enum;
+
+public enum EnumRespType
+{
+    [Description("Success")] MS,
+    [Description("Information")] MI,
+    [Description("Warning")] MW,
+    [Description("Error")] ME,
+    [Description("System Error")] SE,
+    [Description("No Data")] ND
+}
